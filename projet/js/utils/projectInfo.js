727a4a183d41e34b2bb40af05e071f09f563dd3a
yann tyb <yanntyb.lbc@gmail.com>
2021-05-31 16:40:33 +0200

    github stats start

--- projet/js/utils/projectInfo.js
+++ projet/js/utils/projectInfo.js
@@ -1,8 +1,15 @@
 import {Request} from "../classes/Request.js";
 import {MessageAll} from "../classes/MessageAll.js";
 import {getDate} from "./fonctionUtils.js";
+import {ChannelAll} from "../classes/ChannelAll.js";
+import {ChannelSingle} from "../classes/ChannelSingle.js";
 
 let reqProjectName = new Request("project/get.php", setProjectName);
+
+let events = [];
+let morePage = true
+let page = 1;
+let next = true;
 
 let infos = document.getElementsByClassName("infoServe");
 
@@ -16,11 +23,23 @@ for(let info of infos){
 
 function setProjectName(data){
     console.log(data);
-    console.log()
-    let reqInfo = new Request("", callbackInfo);
-    reqInfo.get("https://api.github.com/repos/" + data.link + "/events");
+    morePage = true;
+    next = true;
+    while(morePage && next){
+        let reqInfo = new Request("", callbackInfo);
+        reqInfo.get("https://api.github.com/repos/" + data.link + "/events?page=" + page);
+        page ++;
+        next = false;
+    }
+
 }
 
 function callbackInfo(data){
-    console.log(data)
+    console.log(data);
+    if(data === []){
+        morePage = false;
+    }
+    else{
+        next = true;
+    }
 }
